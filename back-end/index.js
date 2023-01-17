761ad683808348500b358143eda2457bfebaf832
SumitUjjwal <sumitonboard@gmail.com>
2023-01-17 11:25:13 +0530

    provided user route

--- back-end/index.js
+++ back-end/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-
 require("dotenv").config();
 const port = process.env.port;
+
 const { connect } = require("./config/db");
+
+// routes
+const {userRouter} = require("./routes/user.route");
 
 const app = express();
 
+app.get("/", (req, res) => {
+       res.json({"msg": "Welcome to CarZone"});
+})
+
+app.use("/user",userRouter);
 
 app.listen(port, async () => {
        try {
@@ -16,6 +24,6 @@ app.listen(port, async () => {
        } catch (error) {
               console.log(error);
               console.log("error message: ", error.message);
-              console.log("*****error in connecting to database*****")
+              console.log("*****error in connecting to database*****");
        }
 })
