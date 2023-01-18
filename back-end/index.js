98e2abda2562467a742dad382abf2e36e2e3accc
SumitUjjwal <sumitonboard@gmail.com>
2023-01-18 23:10:00 +0530

    day-2

--- back-end/index.js
+++ back-end/index.js
@@ -6,15 +6,17 @@ const port = process.env.port;
 const { connect } = require("./config/db");
 
 // routes
-const {userRouter} = require("./routes/user.route");
+const { userRouter } = require("./routes/user.route");
+const { productRouter } = require("./routes/product.route");
 
 const app = express();
-
+app.use(cors());
 app.get("/", (req, res) => {
-       res.json({"msg": "Welcome to CarZone"});
+       res.json({ "msg": "Welcome to CarZone" });
 })
 
-app.use("/user",userRouter);
+app.use("/user", userRouter);
+app.use("/product", productRouter);
 
 app.listen(port, async () => {
        try {
@@ -27,3 +29,4 @@ app.listen(port, async () => {
               console.log("*****error in connecting to database*****");
        }
 })
+
