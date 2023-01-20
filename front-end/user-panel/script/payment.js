63a774f8bb5178dfd0529bbc44b5f92361a3998e
SumitUjjwal <sumitonboard@gmail.com>
2023-01-21 01:20:55 +0530

    posting order placed data to the server

--- front-end/user-panel/script/payment.js
+++ front-end/user-panel/script/payment.js
@@ -1,7 +1,31 @@
 let payment_button = document.getElementById("payment_button");
 
-payment_button.addEventListener("click", function () {
+
+payment_button.addEventListener("click", async function (event) {
+       let customerId = (localStorage.getItem("userId"));
+       let productId = JSON.parse(localStorage.getItem("cartItem"));
+       let totalPrice = JSON.parse(localStorage.getItem("totalPrice"));
+       let status = "placed";
+
+       let obj = {
+              customerId,
+              productId,
+              totalPrice,
+              status
+       }
+
+       console.log(obj);
+       
+       let request = fetch(`http://localhost:8080/order/add`, {
+              method: "POST",
+              headers: {
+                     "Content-Type": "application/json"
+              },
+              body: JSON.stringify(obj)
+       })
+
        alert("Order Placed Successfully");
+
        localStorage.removeItem("cartItem");
        localStorage.removeItem("combPrice");
        localStorage.removeItem("totalPrice");
